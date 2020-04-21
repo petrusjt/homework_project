67ad85364dc52802c361241ea33f70b38cb8b5e8
petrusjt <jozsefpetrus99@gmail.com>
2020-04-21 13:18:07 +0200

    Added game window creation to src/main/java/gui/MainWindowController

--- src/main/java/gui/MainWindowController.java
+++ src/main/java/gui/MainWindowController.java
@@ -2,23 +2,45 @@ package gui;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
 
-public class MainWindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MainWindowController implements Initializable {
     @FXML
-    Button btn_start;
+    private AnchorPane pane_anchor;
 
     @FXML
-    Button btn_highscores;
+    private Button btn_start;
 
     @FXML
-    Button btn_exit;
+    private Button btn_highscores;
+
+    @FXML
+    private Button btn_exit;
 
     @FXML
     protected void startGame(MouseEvent event)
     {
-        //TODO create and open new window containing the game
+        CanvasPane gameLayout = new CanvasPane(800,600);
+        Scene gameScene = new Scene(gameLayout, 800,600);
+        Stage gameStage = new Stage();
+        gameStage.setResizable(false);
+        gameStage.setTitle("Game Window");
+        gameStage.initModality(Modality.WINDOW_MODAL);
+        gameStage.initOwner(pane_anchor.getScene().getWindow());
+        gameStage.setScene(gameScene);
+        gameStage.show();
+
     }
 
     @FXML
@@ -30,7 +52,13 @@ public class MainWindowController {
     @FXML
     protected void exitGame(MouseEvent event)
     {
+        Logger.info("Application exit");
         Platform.exit();
     }
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
