3424517c38cf1d7cb0b7681fde2247d1ca6686c1
petrusjt <jozsefpetrus99@gmail.com>
2020-05-17 19:31:11 +0200

    Implemented the game's logic

--- src/main/java/game/controller/GameController.java
+++ src/main/java/game/controller/GameController.java
@@ -10,13 +10,16 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import org.tinylog.Logger;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,69 +34,295 @@ import xmlhelper.JAXBHelper;
  * Class for controlling the game.
  * */
 public class GameController {
-    private Canvas canvas;
     private Player player;
     private Monster monster;
     private List<Wall> walls;
     private Grid grid;
-    GraphicsDrawer graphicsDrawer;
+    private GraphicsDrawer graphicsDrawer;
 
-    public GameController() {
+    private final int NUMBER_OF_CELLS = 6;
 
+    private List<KeyCode> moveKeys;
+
+    private enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
     }
 
     public GameController(Canvas canvas) {
-        grid = new Grid(canvas.getWidth(), canvas.getHeight(),6);
-        this.canvas = canvas;
+        grid = new Grid(canvas.getWidth(), canvas.getHeight(), NUMBER_OF_CELLS);
         this.player = new Player(new Point(0,0), (float) (Math.min(grid.getCellHeight(), grid.getCellWidth())) / 2);
         this.monster = new Monster(new Point(4,2),(float) (Math.min(grid.getCellHeight(), grid.getCellWidth())) / 2);
         this.walls = new ArrayList<>();
         this.graphicsDrawer = new GraphicsDrawer(player,monster,walls,canvas,grid);
+
+        moveKeys = new ArrayList<>();
+        moveKeys.add(KeyCode.W);
+        moveKeys.add(KeyCode.S);
+        moveKeys.add(KeyCode.A);
+        moveKeys.add(KeyCode.D);
+        moveKeys.add(KeyCode.UP);
+        moveKeys.add(KeyCode.DOWN);
+        moveKeys.add(KeyCode.LEFT);
+        moveKeys.add(KeyCode.RIGHT);
+
         try {
             loadMap();
         } catch (ParserConfigurationException | IOException | SAXException | JAXBException e) {
             e.printStackTrace();
         }
 
-        this.graphicsDrawer.draw();
+        if(!doesSaveFileExist())
+        {
+            try {
+                createSaveFile();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        graphicsDrawer.draw();
+    }
+
+    public void handleKeyEvent(KeyEvent keyEvent) {
+        Logger.debug("Key pressed: " + keyEvent.getCode());
+        doGameLogic(keyEvent);
+    }
+
+    private void doGameLogic(KeyEvent keyEvent) {
+        KeyCode code = keyEvent.getCode();
+        if(moveKeys.contains(code))
+        {
+            if(canPlayerMove(getDirectionFromKeyCode(code)))
+            {
+                if(!isWinningMove(getDirectionFromKeyCode(code)))
+                {
+                    movePlayer(getDirectionFromKeyCode(code));
+                    moveMonster();
+                    graphicsDrawer.draw();
+                    if(isGameLost())
+                    {
+                        handleGameOver(keyEvent);
+                    }
+                }
+                else
+                {
+                    handleGameOver(keyEvent);
+                }
+            }
+        }
+
+    }
+
+    private boolean canPlayerMove(Direction direction) {
+        Point2D playerPosition = player.getPosition();
+        Point wallStart = new Point(0,0);
+        Point wallEnd = new Point(0,0);
+        if(direction == Direction.UP)
+        {
+            wallStart.x = (int) playerPosition.getX();
+            wallStart.y = (int) playerPosition.getY();
+            wallEnd.x = (int) (playerPosition.getX() + 1);
+            wallEnd.y = (int) playerPosition.getY();
+        }
+        else if(direction == Direction.DOWN)
+        {
+            wallStart.x = (int) playerPosition.getX();
+            wallStart.y = (int) (playerPosition.getY() + 1);
+            wallEnd.x = (int) (playerPosition.getX() + 1);
+            wallEnd.y = (int) (playerPosition.getY() + 1);
+        }
+        else if(direction == Direction.LEFT)
+        {
+            wallStart.x = (int) playerPosition.getX();
+            wallStart.y = (int) playerPosition.getY();
+            wallEnd.x = (int) playerPosition.getX();
+            wallEnd.y = (int) (playerPosition.getY() + 1);
+        }
+        else if(direction == Direction.RIGHT)
+        {
+            wallStart.x = (int) (playerPosition.getX() + 1);
+            wallStart.y = (int) playerPosition.getY();
+            wallEnd.x = (int) (playerPosition.getX() + 1);
+            wallEnd.y = (int) (playerPosition.getY() + 1);
+        }
+
+        //TODO: find a better name for this object
+        Wall checkWall = new Wall(wallStart,wallEnd);
+
+        boolean canMove = canMove = !walls.contains(checkWall);
+
+        if(canMove)
+            Logger.debug("Did not find wall so Player can move in that direction");
+        else
+            Logger.debug("Found wall so Player cannot move in that direction");
+
+        return canMove;
+    }
+
+    private void movePlayer(Direction direction) {
+        Point2D playerPosition = player.getPosition();
+        Point newPlayerPosition = new Point();
+        if(direction == Direction.UP)
+        {
+            newPlayerPosition.x = (int) playerPosition.getX();
+            newPlayerPosition.y = (int) playerPosition.getY() - 1;
+        }
+        else if(direction == Direction.DOWN)
+        {
+            newPlayerPosition.x = (int) playerPosition.getX();
+            newPlayerPosition.y = (int) playerPosition.getY() + 1;
+        }
+        else if(direction == Direction.LEFT)
+        {
+            newPlayerPosition.x = (int) playerPosition.getX() - 1;
+            newPlayerPosition.y = (int) playerPosition.getY();
+        }
+        else if(direction == Direction.RIGHT)
+        {
+            newPlayerPosition.x = (int) playerPosition.getX() + 1;
+            newPlayerPosition.y = (int) playerPosition.getY();
+        }
+        player.setPosition(newPlayerPosition);
+    }
+
+    private void moveMonster() {
+        moveMonsterOnce();
+        moveMonsterOnce();
+    }
+
+    private boolean isGameLost() {
+        return player.getPosition().equals(monster.getPosition());
+    }
+
+    private void moveMonsterOnce() {
+        Point2D playerPosition = player.getPosition();
+        Point2D monsterPosition = monster.getPosition();
+        if(playerPosition.getX() < monsterPosition.getX() && canMonsterMove(Direction.LEFT))
+        {
+            monsterPosition.setLocation(new Point((int) monsterPosition.getX() - 1, (int) monsterPosition.getY()));
+        }
+        else if(playerPosition.getX() > monsterPosition.getX() && canMonsterMove(Direction.RIGHT))
+        {
+            monsterPosition.setLocation(new Point((int) monsterPosition.getX() + 1, (int) monsterPosition.getY()));
+        }
+        else if(playerPosition.getY() < monsterPosition.getY() && canMonsterMove(Direction.UP))
+        {
+            monsterPosition.setLocation(new Point((int) monsterPosition.getX(), (int) monsterPosition.getY() - 1));
+        }
+        else if(playerPosition.getY() > monsterPosition.getY() && canMonsterMove(Direction.DOWN))
+        {
+            monsterPosition.setLocation(new Point((int) monsterPosition.getX(), (int) monsterPosition.getY() + 1));
+        }
+
+    }
+
+    private boolean canMonsterMove(Direction direction) {
+        Point2D monsterPosition = monster.getPosition();
+        Point wallStart = new Point(0,0);
+        Point wallEnd = new Point(0,0);
+        if(direction == Direction.UP)
+        {
+            wallStart.x = (int) monsterPosition.getX();
+            wallStart.y = (int) monsterPosition.getY();
+            wallEnd.x = (int) monsterPosition.getX() + 1;
+            wallEnd.y = (int) monsterPosition.getY();
+        }
+        else if(direction == Direction.DOWN)
+        {
+            wallStart.x = (int) monsterPosition.getX();
+            wallStart.y = (int) (monsterPosition.getY() + 1);
+            wallEnd.x = (int) (monsterPosition.getX() + 1);
+            wallEnd.y = (int) (monsterPosition.getY() + 1);
+        }
+        else if(direction == Direction.LEFT)
+        {
+            wallStart.x = (int) monsterPosition.getX();
+            wallStart.y = (int) monsterPosition.getY();
+            wallEnd.x = (int) monsterPosition.getX();
+            wallEnd.y = (int) (monsterPosition.getY() + 1);
+        }
+        else if(direction == Direction.RIGHT)
+        {
+            wallStart.x = (int) (monsterPosition.getX() + 1);
+            wallStart.y = (int) monsterPosition.getY();
+            wallEnd.x = (int) (monsterPosition.getX() + 1);
+            wallEnd.y = (int) (monsterPosition.getY() + 1);
+        }
+
+        //TODO: find a better name for this object
+        Wall checkWall = new Wall(wallStart,wallEnd);
+
+        boolean canMove = canMove = !walls.contains(checkWall);
+
+        if(canMove)
+            Logger.debug("Did not find wall so Monster can move in the tested direction");
+        else
+            Logger.debug("Found wall so Monster cannot move in the tested direction");
+
+        return canMove;
+    }
+
+    private boolean isWinningMove(Direction direction) {
+        if(direction == Direction.UP)
+        {
+            if(player.getPosition().getY() == 0)
+            {
+                return true;
+            }
+        }
+        else if(direction == Direction.DOWN)
+        {
+            if(player.getPosition().getY() == NUMBER_OF_CELLS)
+            {
+                return true;
+            }
+        }
+        else if(direction == Direction.LEFT)
+        {
+            if(player.getPosition().getX() == 0)
+            {
+                return true;
+            }
+        }
+        else if(direction == Direction.RIGHT)
+        {
+            if(player.getPosition().getY() == NUMBER_OF_CELLS)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void loadMap() throws ParserConfigurationException, IOException, SAXException, JAXBException {
         game.controller.mapxmlreader.Walls walls = JAXBHelper.fromXML(game.controller.mapxmlreader.Walls.class, getClass().getClassLoader().getResourceAsStream("maps/map.xml"));
         for(game.controller.mapxmlreader.Wall wall : walls.getWall())
         {
-            System.out.println(wall);
             this.walls.add(new Wall(wall.getStart().getX(),wall.getStart().getY(), wall.getEnd().getX(), wall.getEnd().getY()));
         }
         graphicsDrawer.setWalls(this.walls);
-        System.out.println(walls);
 
     }
 
-    public void handleKeyEvent(KeyEvent keyEvent) {
-        Logger.debug("Key pressed: " + keyEvent.getCode());
-
-        if(keyEvent.getCode().equals(KeyCode.Q))
+    private void handleGameOver(KeyEvent keyEvent) {
+        if(isGameLost())
         {
-            resetMainWindow((Stage) ((Scene)keyEvent.getSource()).getWindow());
+            Logger.debug("Player lost the game.");
         }
-        graphicsDrawer.draw();
+        else
+        {
+            Logger.debug("Player won the game.");
+        }
+        savePlayerStats();
+
+        resetMainWindow((Stage) ((Scene) keyEvent.getSource()).getWindow());
     }
 
-    public void setCanvas(Canvas canvas) {
-        this.canvas = canvas;
-    }
-
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-
-    public void setMonster(Monster monster) {
-        this.monster = monster;
-    }
-
-    public void setWalls(List<Wall> walls) {
-        this.walls = walls;
+    private void savePlayerStats() {
+        //TODO
     }
 
     private void resetMainWindow(Stage stage)
@@ -110,6 +339,54 @@ public class GameController {
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private boolean doesSaveFileExist()
+    {
+        Logger.info("Checking for save file");
+
+        String userHome = System.getProperty("user.home");
+        String separator = File.separator;
+        String saveFilePath = userHome + separator + ".labyrinth" + separator + "savefile.xml";
+        Logger.debug("Save file location: " + saveFilePath);
+
+        File saveFile = new File(saveFilePath);
+        return saveFile.exists();
+    }
+
+    private void createSaveFile() throws FileNotFoundException {
+        Logger.info("Save file not found. Creating save file.");
+
+        String userHome = System.getProperty("user.home");
+        String separator = File.separator;
+        String saveFilePath = userHome + separator + ".labyrinth" + separator + "savefile.xml";
+        String saveFileDir = userHome + separator + ".labyrinth";
+        new File(saveFileDir).mkdirs();
+        File saveFile = new File(saveFilePath);
+        PrintWriter printWriter = new PrintWriter(saveFile);
+        printWriter.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        printWriter.println("<highscores></highscores>");
+        printWriter.close();
+
+    }
+
+    private Direction getDirectionFromKeyCode(KeyCode code) {
+        if(code.equals(KeyCode.W) || code.equals(KeyCode.UP))
+        {
+            return Direction.UP;
+        }
+        else if(code.equals(KeyCode.S) || code.equals(KeyCode.DOWN))
+        {
+            return Direction.DOWN;
+        }
+        else if(code.equals(KeyCode.A) || code.equals(KeyCode.LEFT))
+        {
+            return Direction.LEFT;
+        }
+        else
+        {
+            return Direction.RIGHT;
+        }
     }
 
     public void handleOnCloseRequest(WindowEvent event) {
