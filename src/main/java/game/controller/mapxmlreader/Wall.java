7941f59cef7507de7d4011bdb3561e0c3ef6e6cd
petrusjt <jozsefpetrus99@gmail.com>
2020-05-20 07:34:36 +0200

    Removed unnecessary annotation

--- src/main/java/game/controller/mapxmlreader/Wall.java
+++ src/main/java/game/controller/mapxmlreader/Wall.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlValue;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @Data
