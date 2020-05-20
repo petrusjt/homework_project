5bc616d9882efbd3781b104233d13b73f0ceb53c
petrusjt <jozsefpetrus99@gmail.com>
2020-05-20 13:55:27 +0200

    Implemented methods initialize(URL,ResourceBundle) and backTOMainMenu(MouseEvent)

--- src/main/java/gui/HighScoreWindowController.java
+++ src/main/java/gui/HighScoreWindowController.java
@@ -1,13 +1,92 @@
 package gui;
 
+import game.highscore.HighScore;
+import game.highscore.HighScores;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
+import xmlhelper.JAXBHelper;
 
+import javax.xml.bind.JAXBException;
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HighScoreWindowController implements Initializable {
+    @FXML
+    private TableView<HighScore> highScoreTable;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        String userHome = System.getProperty("user.home");
+        String separator = File.separator;
+        String saveFilePath = userHome + separator + ".labyrinth" + separator + "savefile.xml";
+        Logger.info("Loading save file data from " + saveFilePath);
 
+        File saveFile = new File(saveFilePath);
+
+        try {
+            InputStream inputStream = new FileInputStream(saveFile);
+            HighScores highScores = JAXBHelper.fromXML(HighScores.class, inputStream);
+
+            TableColumn<HighScore, String> userNameCol = new TableColumn<>("Név");
+            userNameCol.setPrefWidth(150);
+            userNameCol.setStyle("-fx-alignment: CENTER");
+            TableColumn<HighScore, Float> timeCol = new TableColumn<>("Idő");
+            timeCol.setPrefWidth(150);
+            timeCol.setStyle("-fx-alignment: CENTER");
+            TableColumn<HighScore, Integer> stepsCol = new TableColumn<>("Lépések száma");
+            stepsCol.setPrefWidth(150);
+            stepsCol.setStyle("-fx-alignment: CENTER");
+            TableColumn<HighScore, String> isFinishedCol = new TableColumn<>("Kijutott?");
+            isFinishedCol.setPrefWidth(150);
+            isFinishedCol.setStyle("-fx-alignment: CENTER");
+
+            userNameCol.setCellValueFactory(new PropertyValueFactory<>("playerName"));
+            timeCol.setCellValueFactory(new PropertyValueFactory<>("secondsToEnd"));
+            stepsCol.setCellValueFactory(new PropertyValueFactory<>("numberOfSteps"));
+            isFinishedCol.setCellValueFactory(new PropertyValueFactory<>("gameWon"));
+
+            highScoreTable.getColumns().addAll(userNameCol,timeCol,stepsCol,isFinishedCol);
+
+            for(HighScore highScore : highScores.getHighScores())
+            {
+                highScoreTable.getItems().add(highScore);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Logger.warn("Save file not found at " + saveFilePath);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void backToMainMenu(MouseEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(FXMLLoader.getDefaultClassLoader().getResource("fxml/main_menu.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Főmenü");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 }
